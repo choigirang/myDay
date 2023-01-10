f41fccb8115d797ea01406a5f7e1827df7eb3639
choigirang <choigirang@kakao.com>
2023-01-11 00:21:45 +0900

    test

---
+++ script.js
@@ -0,0 +1,60 @@
+[33mcommit 008f0dfab694ff2710f675a272fb6e460f68ca3a[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: choigirang <choigirang@kakao.com>
+Date:   Tue Jan 10 22:39:00 2023 +0900
+
+    img done
+
+[33mcommit 4a7d16d50387ae4bfe7e18fa98cd6f833ea20e20[m
+Merge: 69b3f2a c5aedbd
+Author: choigirang <choigirang@kakao.com>
+Date:   Tue Jan 10 22:31:08 2023 +0900
+
+    Merge branch 'main' of github.com:choigirang/myDay
+
+[33mcommit 69b3f2af13e09e8dda29f144152fe82990527ffe[m
+Author: choigirang <choigirang@kakao.com>
+Date:   Tue Jan 10 22:29:57 2023 +0900
+
+    fonts
+
+[33mcommit d648c8cbe7e489d1e5616372dbfa96d378b9de68[m
+Author: choigirang <choigirang@kakao.com>
+Date:   Tue Jan 10 22:27:38 2023 +0900
+
+    fonts
+
+[33mcommit c5aedbde925169df4c26d5d768826e1574400356[m[33m ([m[1;31morigin/main[m[33m)[m
+Author: choigirang <118104644+choigirang@users.noreply.github.com>
+Date:   Tue Jan 10 03:17:41 2023 +0900
+
+    Delete script2.js
+
+[33mcommit 4197fa8df45c72c35a1304e3459a95fb26ad8d1e[m
+Author: choigirang <choigirang@kakao.com>
+Date:   Tue Jan 10 03:13:42 2023 +0900
+
+    img_function
+
+[33mcommit 63141f75aa99552795b04956ea632fa3e15a2fea[m
+Author: choigirang <choigirang@kakao.com>
+Date:   Tue Jan 10 03:10:49 2023 +0900
+
+    Implement write content,submit,data lender,linked data
+
+[33mcommit e997bb25f36f62c95b9b449c2d6b3c2c74a1afcf[m
+Author: choigirang <choigirang@kakao.com>
+Date:   Mon Jan 9 00:07:35 2023 +0900
+
+    second modify
+
+[33mcommit 82430bfe8db5eb2d230955241b639305dafefbb9[m
+Author: choigirang <choigirang@kakao.com>
+Date:   Sun Jan 8 19:33:57 2023 +0900
+
+    second
+
+[33mcommit f2572c1bf921ce804a7f248970bf24e4422e7846[m
+Author: choigirang <choigirang@kakao.com>
+Date:   Sun Jan 8 16:18:17 2023 +0900
+
+    first one
